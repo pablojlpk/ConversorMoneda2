30c8e976dbfba80f11dfb13a70ae82d1b3086a9d
pablojlpk <113646557+pablojlpk@users.noreply.github.com>
2023-08-24 08:14:33 -0300

    -

--- app/src/main/java/com/example/conversormoneda2/MainActivity.java
+++ app/src/main/java/com/example/conversormoneda2/MainActivity.java
@@ -1,18 +1,17 @@
 package com.example.conversormoneda2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.conversormoneda2.databinding.ActivityMainBinding;
 
-import java.util.Objects;
-
 public class MainActivity extends AppCompatActivity {
 private MainActivityViewModel vm;
+private int tconversion=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,28 +19,40 @@ private MainActivityViewModel vm;
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         vm = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+        vm.getMutableConversion().observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+            binding.texResultado.setText(aDouble.toString());
+            }
+        });
 
         binding.btConvertir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                binding.texResultado.setText(vm.calcularConversion(Double.parseDouble(binding.EdDolares.getText().toString()), Double.parseDouble(binding.EdEuros.getText().toString()))+"");
+             vm.calcularConversion((binding.EdDolares.getText().toString()),(binding.EdEuros.getText().toString()),tconversion);
+
             }
+
         });
 
 
-        binding.rbEuroaPesos.setOnClickListener(new View.OnClickListener() {
+        binding.rbEuroaDolares.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //binding.rbPesosaEuros.setChecked(false);
-
-
-            }
+                binding.rbDolaresaEuros.setChecked(false);
+                binding.EdEuros.setEnabled(false);
+                binding.EdDolares.setEnabled(true);
+                tconversion=1;
+}
         });
-        binding.rbPesosaEuros.setOnClickListener(new View.OnClickListener() {
+
+        binding.rbDolaresaEuros.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                binding.rbEuroaPesos.setChecked(false);
-
+                binding.rbEuroaDolares.setChecked(false);
+                binding.EdDolares.setEnabled(false);
+                binding.EdEuros.setEnabled(true);
+                tconversion = 2;
             }
         });
 
--- app/src/main/java/com/example/conversormoneda2/MainActivityViewModel.java
+++ app/src/main/java/com/example/conversormoneda2/MainActivityViewModel.java
@@ -10,27 +10,49 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import java.security.cert.PKIXRevocationChecker;
 
-
 public class MainActivityViewModel extends AndroidViewModel {
     private Context context;
+    private Double cotizaEuro= 1.025;
+    private Double cotizaDolar=1.087;
+
+
+
+
+    private MutableLiveData <Double> mutableconversion;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
         context = application.getApplicationContext();
     }
 
-    public Double calcularConversion(double valor1, double valor2){
-        if (valor1 == 0) {
+    public LiveData<Double> getMutableConversion(){
+        if (mutableconversion == null) {
+            mutableconversion = new MutableLiveData<>();
 
         }
+        return  mutableconversion;
+    }
 
-        
-        double calculo=valor1*valor2;
-        return calculo;
-        //hola
+    public void calcularConversion(String valor1, String valor2,int tconv){
+        Double calculo= (double) 0;
+
+        if (valor1.length()==0 || valor2.length()==0 && tconv==0) {
+            Toast.makeText(context, "RECUERDE INGRESAR VALOR Y EL TIPO DE CONVERSIÓN", Toast.LENGTH_LONG).show();
+        }else {
+            if (tconv==1) {
+                calculo=Double.parseDouble(valor1)*cotizaDolar;
+            } else {
+                calculo=Double.parseDouble(valor2)*cotizaEuro;
+            }
+
+        }
+        mutableconversion.setValue(calculo);
+
     }
 
 
